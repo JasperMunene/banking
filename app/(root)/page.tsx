5cc37781a438301f9688dc96d1e8f74b72aabbfe
JasperMunene <jaspermunene2022@gmail.com>
2024-05-17 22:23:28 +0300

    Implement complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = { firstName: 'Jasper'}
+  const loggedIn = { firstName: 'Jasper', lastName: 'Munene', email: 'devjaspermunene@gmail.com'}
   return (
     <section className='home'>
       <div className='home-content'>
@@ -21,7 +22,15 @@ const Home = () => {
           totalCurrentBalance = {1250.35}
           />
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar
+      user={loggedIn} 
+      transactions = {[]}
+      banks = {[{currentBalance: 123.50}, {currentBalance: 540.20}]}
+      />
     </section>
   )
 }
--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -6,7 +6,7 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const DoughnutChart = ({ accounts }) => {
+const DoughnutChart = ({ accounts } : DoughnutChartProps) => {
   const data = {
     datasets: [
       {
